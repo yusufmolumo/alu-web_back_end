0e766b8378e9339b1a73e0df3e8fd406a7edfb40
yusufmolumo <y.molumo@alustudent.com>
2025-06-24 22:41:11 +0200

    updates

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const result = [];
-    for (const item of array) {
-      result.push(appendString + item);
-    }
-  
-    return result;
+  const result = [];
+  for (const item of array) {
+    result.push(appendString + item);
   }
+
+  return result;
+}
--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,10 +1,10 @@
 export default function createReportObject(employeesList) {
-    return {
-      allEmployees: {
-        ...employeesList,
-      },
-      getNumberOfDepartments() {
-        return Object.keys(employeesList).length;
-      },
-    };
-  }
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments() {
+      return Object.keys(employeesList).length;
+    },
+  };
+}
